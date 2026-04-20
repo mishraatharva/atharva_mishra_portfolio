b196e05cdbe2f6fd99e77d1196c84f1a2d2e6254
mishraatharva <mishraatharva825@gmail.com>
2026-04-20 15:11:46 +0530

    resume updated

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,7 +4,7 @@ import "./Resume.css";
 function Resume() {
     const [loading, setLoading] = useState(true);
 
-    const resumeUrl = "https://drive.google.com/file/d/1FR7gX4XeMgIHZAQpabkd-yaMzk0k8hdG/view?usp=sharing";
+    const resumeUrl = "https://drive.google.com/file/d/1FR7gX4XeMgIHZAQpabkd-yaMzk0k8hdG/view?usp=drive_link";
 
     return (
         <div style={{ position: "relative", height: "100vh" }}>
